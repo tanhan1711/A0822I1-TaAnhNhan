fbb1152ef9a32cd9b97ceec8ccf266a61c926f87
tanhan1711 <112400198+tanhan1711@users.noreply.github.com>
2022-12-11 22:00:57 +0700

    Add files via upload

---
+++ module2/ss08/exercise/Main.java
@@ -0,0 +1,14 @@
+package ss08.exercise;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String result = TennisGame.getScore(4, 13);
+        System.out.println(result);
+        result = TennisGame.getScore(4, 4);
+        System.out.println(result);
+        result = TennisGame.getScore(4, 2);
+        System.out.println(result);
+    }
+}
